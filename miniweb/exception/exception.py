b2ebf3c8cbee8923ee377482f6d220271452b3db
masatma1 <masatma1@fel.cvut.cz>
2021-02-17 22:35:56 +0100

    implementation of exceptions classes

---
+++ miniweb/exception/exception.py
@@ -0,0 +1,19 @@
+class FileException(Exception):
+    """Exception for file operations"""
+    pass
+
+class SingletonExpcetion(Exception):
+    """Exception for creating more than one instances of singleton class"""
+    pass
+
+class ConfigParamsException(Exception):
+    """Exception for missing config parameters"""
+    pass
+
+class CompileRegexException(Exception):
+    """Exception for compiling regex in route class"""
+    pass
+
+class HeaderException(Exception):
+    """Request header exception"""
+    pass
