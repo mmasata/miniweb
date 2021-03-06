476aebf6a908356a8a99f533115ad946880f82fb
masatma1 <masatma1@fel.cvut.cz>
2021-03-06 01:08:14 +0100

    remove files from repository

---
+++ main.py
@@ -0,0 +1,53 @@
+from miniweb import miniweb, Controller, filter, Method, Mime, Status, Log
+
+params = {
+    "port": 8000,
+    "log": Log.DEBUG,
+    "host": "localhost"
+}
+
+app = miniweb(params)
+userController = Controller("/user/")
+
+#filter vraci boolean, pokud chce poslat i nejakou response naplni ji a vrati False
+@filter()
+def globalFilter(req, res):
+    #implementace logiky middleware
+    #res.status(Status.FORBIDDEN).type(Mime.HTML).entity("Neprosel middleware!").build()
+    return True
+
+@filter(userController)
+def isLoggedIn(req, res):
+    #implementace logiky middleware
+    return True
+
+@app.route("/alone", [Method.GET, Method.DELETE])
+def noClassFc(req, res):
+    res.status(Status.BAD_GATEWAY).build()
+
+@app.get("foo", userController)
+def foo(req, res):
+    res.status(Status.OK).type(Mime.HTML).entity("foo").build()
+
+
+@app.post("name/{name}/surname/{surname}", consumes=[Mime.FormData], controller=userController)
+def bar(req, res, var):
+    query_params_string = ""
+    path_params_string = ""
+    for key in req.params:
+        query_params_string += "["+key+"]: "+req.params[key]+"<br>"
+    for key in var:
+        path_params_string += "[" + key + "]: " + var[key] + "<br>"
+    #print(req.content["key"])
+    #print(req.content.key)
+    #print(req.content.multilinekey)
+    #print(type(req.content.file))
+    result = """Path variable jede
+    <br> QueryParams: <br>"""+query_params_string+"""
+    <br> Path params: <br>"""+path_params_string+"""
+    """
+    res.status(Status.ACCEPTED).type(Mime.HTML).entity(result).build()
+
+
+app.run()
+
