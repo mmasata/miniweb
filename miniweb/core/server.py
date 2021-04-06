d877b9a197d30721fd9dea716a4c77c458126e62
masatma1 <masatma1@fel.cvut.cz>
2021-04-06 16:56:56 +0200

    automatic parsing of dictionary to json in response object; ignore empty requests; buffer size in configuration

--- miniweb/core/server.py
+++ miniweb/core/server.py
@@ -4,6 +4,7 @@ from miniweb.message.response import Response
 from miniweb.core.miniweb import log
 from miniweb.exception.exception import *
 import gc
+import ujson
 
 
 class Server:
@@ -23,7 +24,6 @@ class Server:
 
         if Server.__instance is None:
             Server(miniweb)
-        return Server.__instance
 
 
     def __init__(self, miniweb):
@@ -37,33 +37,33 @@ class Server:
 
     def __init(self):
         gc.collect()
-        port = None
-        host = None
+        server_task = None
 
         try:
-            port = self.config.port
-            host = self.config.host
+            server_task = asyncio.start_server(self.__handle, self.config.host, self.config.port)
         except:
             raise ConfigParamsException("Host or port is missing!")
 
-        self.event_loop = asyncio.get_event_loop()
-        self.event_loop.create_task(asyncio.start_server(self.__handle, host, port))
-        log.info("Server is running on "+self.config.host+" port:"+str(port))
-        self.event_loop.run_forever()
+        log.info("Server is running on "+self.config.host+" port:"+str(self.config.port))
+
+        e_loop = asyncio.get_event_loop()
+        e_loop.create_task(server_task)
+        e_loop.run_forever()
 
 
     async def __handle(self, reader, writer):
         req = Request()
-        first_line = True
-        reading_headers = True
+        h_read = True
 
-        while reading_headers:
-            header_line = await reader.readline()
-            if header_line == b"\r\n":
+        while h_read:
+            h_line = await reader.readline()
+
+            if h_line == b"\r\n":
                 log.debug("All headers was accepted.")
                 break
-            reading_headers = await req.parse_header(header_line.decode(), first_line)
-            first_line = False
+
+            if len(h_line) > 0:
+                h_read = await req.parse_header(h_line.decode())
 
         if req.content_read:
             content = await reader.read(-1)
@@ -75,37 +75,43 @@ class Server:
         if res is not None and res.can_send:
             await self.__send_headers(res, writer)
             await self.__send_data(res, writer)
-            await writer.aclose()
-            log.debug("Closing communication with client.")
+            await self.__close_connection(writer)
         else:
             log.warning("End communication with client - will drop on timeout!")
 
 
     async def __send_headers(self, res, writer):
         log.debug("Sending response headers.")
+
         await writer.awrite("HTTP/1.1 "+str(res.stat)+"\r\n")
         await writer.awrite("Content-Type: "+res.mime+"\r\n\r\n")
 
 
     async def __send_data(self, res, writer):
         log.debug("Sending response data")
-        if res.ent.__class__.__name__ == "TextIOWrapper":
-            log.debug("Sending file/s.")
-            b_arr = bytearray(res.buffer_size)
 
+        d_type = res.ent.__class__.__name__
+        if d_type == "TextIOWrapper":
+            log.debug("Sending file/s.")
+
+            b_arr = bytearray(self.config.buffer)
             while True:
                 data = res.ent.readinto(b_arr)
+
                 if not data:
                     break
                 await writer.awrite(b_arr, 0, data)
+
+        elif d_type == "dict":
+            log.debug("Dumps dictionary to JSON string and sending to client.")
+
+            json_str = ujson.dumps(res.ent)
+            await writer.awrite(json_str)
+
         else:
             await writer.awrite(res.ent)
 
 
-    def stop(self):
-        """
-        Stop uasyncio server.
-        :return: None
-        """
-        log.info("Stopping server.")
-        self.event_loop.close()
+    async def __close_connection(self, writer):
+        log.debug("Closing communication with client.")
+        await writer.aclose()
